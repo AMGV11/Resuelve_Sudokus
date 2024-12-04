2384e2b34b4ab60dd9b975129a5a9a3944ce0dad
Antonio Guisado <163998876+AMGV11@users.noreply.github.com>
2024-12-04 18:22:42 +0100

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,15 +1,13 @@
 /**   
    * @autor Antonio Manuel Guisado Valle
-   * @DNI    20061405T
-   * @CentroAsociado Sevilla 
    * 
-   * @title Práctica 2 - Resokucion de puzzles Sudoku con algoritmo VA
+   * @title Resokucion de puzzles Sudoku con algoritmo VA
    * 
    * @describe Resolucion Sudokus:
    * El juego del Sudoku consiste en rellenar un cubo de 9 x 9 celdas dispuestas en 9 subgrupos de 3
-   * x 3 celdas, con números del 1 al 9, atendiendo a la restricción de que no se debe repetir el mismo
-   * número en la misma fila, columna o subgrupo de 9. Un Sudoku dispone de varias celdas con un
-   * valor inicial, de modo que debemos empezar a resolver el problema a partir de esta solución parcial
+   * x 3 celdas, con nÃºmeros del 1 al 9, atendiendo a la restricciÃ³n de que no se debe repetir el mismo
+   * nÃºmero en la misma fila, columna o subgrupo de 9. Un Sudoku dispone de varias celdas con un
+   * valor inicial, de modo que debemos empezar a resolver el problema a partir de esta soluciÃ³n parcial
    * sin modificar ninguna de las celdas iniciales.
 */
 
@@ -21,7 +19,7 @@ public class Sudoku
 		
 		// Inicializacion de la matriz tablero, que albergara nuestro Sudoku
 		int[][] tablero = new int [9][9] ;
-		// Tamaño del tablero
+		// TamaÃ±o del tablero
 		int N = tablero.length;
 		int max = 81;
 		String h = "-h", t = "-t";
@@ -29,13 +27,13 @@ public class Sudoku
 		boolean traza = false;
 		
 		// Switch utilizado a modo de seleccion, dependiendo del numero de argumentos y su orden, para llamar
-		// apropiadamente a los metodos adecuados. Esta diseñada para que se siga obligatoriamente la sintaxis del programa.
+		// apropiadamente a los metodos adecuados. Esta diseÃ±ada para que se siga obligatoriamente la sintaxis del programa.
 		switch (args.length)
 		{
 		// Sin argumentos. Al no dar argumentos, solo queda indicar el error y esperar otra incializacion del programa con argumentos validos.
 		case 0: 
-        	System.out.print("No hay argumentos.");
-        break;
+        		System.out.print("No hay argumentos.");
+       		break;
 		// Caso para la entrada de un solo argumento
 		case 1:
 			// Si el argumento es -h, se imprimira en pantalla la ayuda con la sintaxis, comandos y argumentos
